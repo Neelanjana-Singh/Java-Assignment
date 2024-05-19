8c49f2c909889bda3092874a58893251338363ec
Neelanjana-Singh <93790759+Neelanjana-Singh@users.noreply.github.com>
2024-05-19 14:23:16 +0530

    Update ArrayProduct.java

--- ArrayProduct.java
+++ ArrayProduct.java
@@ -2,38 +2,52 @@ package com.techlabs.array;
 
 import java.util.Scanner;
 
-public class ArrayProduct {
+public class ArrayProduct{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner=new Scanner(System.in);
-		System.out.println("Enter number of elements:");
-		int n=scanner.nextInt();
-		int array[]=new int[n];
-		System.out.println("Enter array elements:");
-		for(int i=0;i<n;i++)
-		{
-			array[i]=scanner.nextInt();
-		}
-		int prod[]=new int[n];
-		int product=1;
-		for(int i=0;i<n;i++)
-		{
-			product=product*array[i];
-		}
-		for(int j=0;j<n;j++)
-		{
-			prod[j]=product/array[j];
-		}
-		 System.out.print("Product array: ");
-	        for (int i = 0; i < n; i++) {
-	            System.out.print(prod[i] + " ");
+	 public static int[] calculateProductArray(int[] array) {
+	        int n = array.length;
+	        int[] prod = new int[n];
+	        int[] left = new int[n];
+	        int[] right = new int[n];
+
+	        left[0] = 1;
+	        for (int i = 1; i < n; i++) {
+	            left[i] = array[i - 1] * left[i - 1];
 	        }
-	        System.out.println();
+
+	        right[n - 1] = 1;
+	        for (int j = n - 2; j >= 0; j--) {
+	            right[j] = array[j + 1] * right[j + 1];
+	        }
+
+	        for (int i = 0; i < n; i++) {
+	            prod[i] = left[i] * right[i];
+	        }
+
+	        return prod;
+	    }
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter the number of elements in the array: ");
+	        int n = scanner.nextInt();
+	        int[] array = new int[n];
+	        if (n <= 1) {
+	            System.out.println("Enter valid array size");
+	        } else {
+	            System.out.println("Enter the elements of the array:");
+	            for (int i = 0; i < n; i++) {
+	                array[i] = scanner.nextInt();
+	            }
+
+	            int[] prod = calculateProductArray(array);
+
+	            System.out.println("The product array is:");
+	            for (int value : prod) {
+	                System.out.print(value + " ");
+	            }
+	        }
 	        scanner.close();
-
-		
-
-	}
-
+    }
 }
